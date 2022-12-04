6600c6c8280aa1bffb2229c3cdb531078ec6c53c
maotek <maoshengj@hotmail.com>
2022-12-04 11:54:04 +0100

    Add day 4

--- days/day04.py
+++ days/day04.py
@@ -2,20 +2,20 @@ from lib import load_input
 
 
 def solve(data, part=1):
-    data = data.splitlines()
+    data = [[list(map(int, b.split("-"))) for b in a] for a in [x.split(",") for x in data.splitlines()]]
     return part_one(data) if part == 1 else part_two(data)
 
 
 def part_one(data):
-    pass
+    return len(list(filter(lambda x: (x[0][0] <= x[1][0] and x[0][1] >= x[1][1]) or (x[1][0] <= x[0][0] and x[1][1] >= x[0][1]), data)))
 
 
 def part_two(data):
-    pass
+    return len([z for z in [set(range(x[0][0], x[0][1]+1)).intersection(range(x[1][0], x[1][1]+1)) for x in data] if z])
 
 
 if __name__ == "__main__":
-    print(solve(load_input("small")))
-    # print(solve(load_input()))
+    # print(solve(load_input("small")))
+    print(solve(load_input()))
     # print(solve(load_input("small"), 2))
-    # print(solve(load_input(), 2))
+    print(solve(load_input(), 2))
